4f43984261babc49486f9ced45d16b766671dda9
shin5ok <shin5ok@55mp.com>
2022-09-04 22:42:05 +0900

    make tidy

--- rails7-with-spanner/main.py
+++ rails7-with-spanner/main.py
@@ -1,23 +1,16 @@
 from diagrams import Cluster, Diagram
-from diagrams.gcp.analytics import BigQuery, Dataflow, PubSub
-from diagrams.gcp.compute import AppEngine, Functions, Run
-from diagrams.gcp.network import *
-from diagrams.gcp.database import BigTable,Memorystore,Spanner
-from diagrams.gcp.operations import Monitoring
-from diagrams.gcp.iot import IotCore
-from diagrams.gcp.storage import GCS
+from diagrams.gcp.analytics import BigQuery
+from diagrams.gcp.compute import Run
+from diagrams.gcp.network import LoadBalancing
+from diagrams.gcp.database import Spanner, Memorystore
 
 with Diagram("Cloud Run with Cloud Spanner", show=True):
 
     lb = LoadBalancing("Google Cloud Load Balancing")
-    with Cluster("Data"):
-        bq = BigQuery("BigQuery")
 
     with Cluster("Application"):
         run = Run("user-api")
         spanner = Spanner("game")
-    
-    monitoring = Monitoring("Logging")
     
     with Cluster("Cache Layer"):
         with Cluster("VPC"):
@@ -26,6 +19,3 @@ with Diagram("Cloud Run with Cloud Spanner", show=True):
     lb >> run
     run >> spanner
     run >> redis
-    run >> monitoring
-    monitoring << bq
-
